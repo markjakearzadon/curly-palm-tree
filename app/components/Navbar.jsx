89d3d83ebba1f10ac69c4b97d230b3f903d95ab1
markjake <markjakearzadon@gmail.com>
2024-02-03 15:31:54 +0800

    anj

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,21 +1,31 @@
-import React from 'react'
+import React from "react";
 
-import Link from "next/link"
+import Link from "next/link";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const session = await getServerSession(authOptions);
+
   return (
-    <nav className='fixed min-w-[600px] p-5 border-b'>
-        <div className='flex justify-between'>
-            <span>
-                <Link href="/">Nigga</Link>
-            </span>
-            <div className='flex gap-x-3'>
-                <Link href="">Sign in</Link>
-                <Link href="">Sign up</Link>
-            </div>
+    <nav className="fixed min-w-[600px] p-5 border-b">
+      <div className="flex justify-between">
+        <span>
+          <Link href="/">Nigga</Link>
+        </span>
+        <div className="flex gap-x-3">
+          {session ? (
+            <Link href="/api/auth/signout">{session.user.name}</Link>
+          ) : (
+            <>
+              <Link href="/api/auth/signin">Sign in</Link>
+              <Link href="">Sign up</Link>
+            </>
+          )}
         </div>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
