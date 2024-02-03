a6171f82212a34981e78296ac1ac697cf9466069
markjake <markjakearzadon@gmail.com>
2024-02-03 23:56:09 +0800

    anj

--- app/[groupId]/layout.jsx
+++ app/[groupId]/layout.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const layout = ({ children }) => {
-  return (
-    <div>{ children }</div>
-  )
-}
+  return <div className="col-span-3">{children}</div>;
+};
 
-export default layout
+export default layout;
--- app/[groupId]/page.jsx
+++ app/[groupId]/page.jsx
@@ -1,13 +1,13 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 
-const GroupMessages = () => {
+const GroupMessages = ({ params: { groupId } }) => {
   const [data, setData] = useState();
 
   useEffect(() => {
     async function getMessages() {
-      fetch(`http://localhost:3000/api/groups/3`)
+      fetch(`http://localhost:3000/api/groups/${parseInt(groupId)}`)
         .then((res) => res.json())
         .then((d) => setData(d));
     }
@@ -15,16 +15,25 @@ const GroupMessages = () => {
   }, []);
 
   return (
-    <div>
+    <div className="col-span-3">
       <ul>
-        {data &&
-          data.messages.map((message) =>
-            message.userId === data.user.id ? (
-              <li key={message.id} className="bg-red-400">{message.message}</li>
-            ) : (
-              <li key={message.id} className="bg-blue-400">{message.message}</li>
-            )
-          )}
+        { !data && (
+          <div className="mt-16 flex flex-col items-center">
+            <span>Fetching</span>
+            <span className="loading loading-ring loading-md"></span>
+          </div>
+        )}
+        {data?.messages.map((message) =>
+          message.userId === data.user.id ? (
+            <li key={message.id} className="bg-red-400">
+              {message.message}
+            </li>
+          ) : (
+            <li key={message.id} className="bg-blue-400">
+              {message.message}
+            </li>
+          )
+        )}
       </ul>
     </div>
   );
